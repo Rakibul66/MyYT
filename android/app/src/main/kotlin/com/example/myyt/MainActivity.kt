bfce6aba60bc4a41f65d88626c13dbad2f3f6971
Md.Rakibul Hasan <32368310+Rakibul66@users.noreply.github.com>
2023-10-18 05:45:56 -0700

    v1
    v1

---
+++ android/app/src/main/kotlin/com/example/myyt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myyt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utilities/keys.dart
@@ -0,0 +1,1 @@
+const String API_KEY = 'AIzaSyAQAAXsauJGm4izq8p1iXX2NuKIzvumvaA';
